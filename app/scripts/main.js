c7e4a6a52f8a582ee2eefde5a8bb8d2d9a24d94d
Matt <mattdye3@gmail.com>
2017-06-05 09:32:22 -0400

    Site WIP

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+(function($){
+  var $navigation = $('.navigation');
+  var $mobileToggle = $('.mobile-toggle');
+  var $content = $('.content');
+  var $heading = $('#about-me .heading');
+
+  $mobileToggle.on('click', function(){
+    $navigation.toggleClass('active');
+    $content.toggleClass('active');
+  })
+
+
+  //Typed
+  var $typed = $heading.find('.typed-container');
+  $typed.typed({
+		strings: [", Engineer", ", Yellow Jacket", ", Developer",  ""],
+		typeSpeed: 40,
+    startDelay: 1500,
+    backDelay: 1500,
+    backSpeed: 40,
+	  callback: function(){
+      $heading.find('span').remove();
+    }
+  });
+
+})(jQuery)
